b923af8b558b610c59c1da68949c5e11102001dd
SREESH BONAGIRI <sreeshbonagiri9062@gmail.com>
2023-09-21 16:21:45 +0530

    Create Remove a Key.py

---
+++ Dictionaries/Remove a Key.py
@@ -0,0 +1,12 @@
+students_dict = {
+    "Ram": "Cricket",
+    "Naresh": "Football",
+    "Vani": "Tennis",
+    "Rahim": "Cricket",
+    "Deepak": "Boxing"
+}
+
+# Write your code here
+a = input()
+del students_dict[a]
+print(students_dict)
